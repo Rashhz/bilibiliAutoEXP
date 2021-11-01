43c2c3102672056de1f1a5e4d1794833fc686f12
Rsaszek <adiuhhuawie@gmail.com>
2021-11-01 12:50:39 +0800

    加了注释

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-import requests
-
 from fun import *
 import random
 
 
 if __name__ == '__main__':
-    getuserInfo()
-    liveSign()
-    comicSign()
+    getuserInfo()            #基本信息
+    liveSign()              #直播签到
+    comicSign()             #漫画签到
     hotvideolist = getHotVideo()
-    sharevideo(aid=hotvideolist[random.randint(1, 50)]['aid'])
-    a = random.randint(1,50)
-    report(hotvideolist[a]['aid'],hotvideolist[a]['cid'],1000)
-    for item in hotvideolist:  #50经验
+    sharevideo(aid=hotvideolist[random.randint(1, 50)]['aid'])      #漫画签到
+    a = random.randint(1, 50)
+    report(hotvideolist[a]['aid'], hotvideolist[a]['cid'], 1000)    #观看视频
+    for item in hotvideolist:  #投币
         if getCoinTodayExp() == 50:
             print(('投币任务已完成，已获得所有投币经验'))
             break
