d350630143654cffb93809246aa2cdaaf7d0b880
Rashhz <75512009+Rashhz@users.noreply.github.com>
2021-11-17 18:07:37 +0800

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,5 +1,5 @@
 # cookie
-setCookie = "_uuid=E2D633B1-C9CC-E37E-6289-3A310D490ED571360infoc; buvid3=8170B38B-AB3D-4D9A-A05F-7A78B2103235167640infoc; fingerprint=05566bff21187560a35e6276c44d9ab7; buvid_fp=8170B38B-AB3D-4D9A-A05F-7A78B2103235167640infoc; buvid_fp_plain=8170B38B-AB3D-4D9A-A05F-7A78B2103235167640infoc; SESSDATA=3163a32c%2C1651034904%2C22bfe%2Aa1; bili_jct=ba87496978ec6cd9a7717d5f7e046eba; DedeUserID=430521080; DedeUserID__ckMd5=ed1a50c814af9670; sid=5ym7ohg9; bp_t_offset_430521080=586876722310080632"
+setCookie = "”
 
 cookies = dict([l.split("=", 1) for l in setCookie.split("; ")])
 bili_jct = cookies['bili_jct']
